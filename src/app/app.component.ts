1f0b4a661a62873c666b2c32551cb4a728a837e1
Chad Foley <chad.foley@raleighnc.gov>
2018-08-27 14:45:56 -0400

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+
+import {Component, OnInit} from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+import {map} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
-export class AppComponent {
-  title = 'mscarlarea2';
+export class AppComponent implements OnInit {
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
+  images: Array<string>;
+
+  constructor(config: NgbCarouselConfig, private _http: HttpClient) {
+    // customize default values of carousels used by this component tree
+      config.showNavigationArrows = true;
+      config.showNavigationIndicators = true;
+  }
+
+  ngOnInit() {
+    this._http.get('https://picsum.photos/list')
+        .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
+        .subscribe(images => this.images = images);
+  }
+
+  private _randomImageUrls(images: Array<{id: number}>): Array<string> {
+    return [1, 2].map(() => {
+      const randomId = images[Math.floor(Math.random() * images.length)].id;
+      return `https://picsum.photos/900/500?image=${randomId}`;
+    });
+  }
 }
